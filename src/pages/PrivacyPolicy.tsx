d2ea166a5c29a5a0d041353bba33bec30e2f22ed
syfaarizal <syifairgi@gmail.com>
2026-06-23 08:37:31 +0700

    fix: Remove unused ChatIcon import from PrivacyPolicy component

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import type { ReactNode } from 'react';
 import { Link } from 'react-router-dom';
-import { ShieldIcon, ChatIcon } from '../components/ThemeIcons';
+import { ShieldIcon } from '../components/ThemeIcons';
 
 const INDIGO = '#818cf8';
 
